834faa3a5e76b551c52a9d29a85810593638aebc
ZHCHUANGSI\huangyao <huangyao@ZHCHUANGSI.com>
2019-03-21 08:25:23 +0800

    Add [AdminLteAspNetCoreMVC1] solution

---
+++ AdminLteAspNetCoreMVC1/AdminLteAspNetCoreMVC1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdminLteAspNetCoreMVC1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
