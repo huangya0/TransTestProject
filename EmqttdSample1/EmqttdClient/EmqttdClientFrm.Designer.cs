5a220a90521715a1fc598c213f350d325cddca83
ZHCHUANGSI\huangyao <huangyao@ZHCHUANGSI.com>
2019-04-03 17:49:45 +0800

    加入EmqttdSample1例子

---
+++ EmqttdSample1/EmqttdClient/EmqttdClientFrm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace EmqttdClient
+{
+    partial class EmqttdClientFrm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(78, 41);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "btnConnect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.txtMsg.Location = new System.Drawing.Point(78, 106);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(552, 303);
+            this.txtMsg.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(384, 41);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "btnSend";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // EmqttdClientFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "EmqttdClientFrm";
+            this.Text = "EmqttdClientFrm";
+            this.Load += new System.EventHandler(this.EmqttdClientFrm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ EmqttdSample1/EmqttdClient/EmqttdClientFrm.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace EmqttdClient
+{
+    public partial class EmqttdClientFrm : Form
+    {
+        public EmqttdClientFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void EmqttdClientFrm_Load(object sender, EventArgs e)
+        {
+            //http://www.emqtt.com/
+            //https://blog.csdn.net/liluyuan/article/details/80445896
+
+            //    2.下载复制到D:盘,进行解压 到emqttd目录,
+
+            //2.1在cmd模式下, 进入D:\emqttd\bin目录: 执行如下命令: emqttd start
+
+            //2.2:执行完成后,可以在浏览器中,打开如下网站: http://127.0.0.1:18083 可以打开什么安装成功
+
+            //        默认用户名: admin 密码:public
+            //--------------------- 
+        
+        }
+
+    public void WriteMsg(string msg)
+        {
+            //string msg = "Topic:" + e.Topic + "   Message:" + System.Text.Encoding.Default.GetString(e.Message);
+
+            this.txtMsg.AppendText("\r\n" + msg);
+        }
+
+        public void messageReceive(object sender, MqttMsgPublishEventArgs e)
+        {
+            string msg = "Topic:" + e.Topic + "   Message:" + System.Text.Encoding.Default.GetString(e.Message);
+            this.WriteMsg(msg);
+        }
+
+        static bool cafileValidCallback(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            string msg = "X509 链状态:";
+            foreach (X509ChainStatus status in chain.ChainStatus)
+            {
+                msg += status.StatusInformation + "\n";
+            }
+            msg += "SSL策略问题：" + (int)sslPolicyErrors;
+
+            Console.WriteLine(msg);
+
+            if (sslPolicyErrors != SslPolicyErrors.None)
+                return false;
+            return true;
+        }
+
+        MqttClient client = null;
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            string brokerHostName = "127.0.0.1";
+            int brokerPort = 8883;
+            string brokerHostUrl = "127.0.0.1";
+            string clientId = Guid.NewGuid().ToString(); //"m2mqtt";
+            string username = "develop";
+            string password = "666666";
+            string[] topic = { "topic" };
+            byte[] qosLevels = { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE };
+            //string caPath = "C:/MqttSSL/ca.crt";
+            string caPath = @"D:\emqttd\zacktest.cer";
+            X509Certificate caCert = new X509Certificate(caPath);
+            Console.WriteLine(caCert.ToString(true) + "\n" + caCert.ToString());
+            Console.ReadKey();
+            Console.WriteLine("------------------------分割线-------------------------------");
+            //无SSL连接
+            //MqttClient client = new MqttClient(brokerHostName,brokerPort,false,null,null,MqttSslProtocols.None);
+            try
+            {
+                //单向SSL通信
+                // MqttClient client = new MqttClient(brokerHostName, brokerPort, true, caCert, null, MqttSslProtocols.TLSv1_2, new RemoteCertificateValidationCallback(cafileValidCallback));
+                //MqttClient client = new MqttClient(brokerHostName, brokerPort, true, null, null, MqttSslProtocols.TLSv1_2);
+                client = new MqttClient(brokerHostUrl);
+                //消息接受
+                client.MqttMsgPublishReceived += new MqttClient.MqttMsgPublishEventHandler(messageReceive);
+                //连接Broker
+                client.Connect(clientId, username, password);
+
+                client.Subscribe(topic, qosLevels);
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine("连接失败! msg : " + ex.Message);
+                Console.ReadKey();
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string topic = "topic";
+            byte[] qosLevels = { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE };
+            //client.Subscribe(topic,  qosLevels);
+
+            string msg = "msg sent from client!Q";
+            client.Publish(topic, System.Text.Encoding.UTF8.GetBytes(msg));
+        }
+    }
+}
