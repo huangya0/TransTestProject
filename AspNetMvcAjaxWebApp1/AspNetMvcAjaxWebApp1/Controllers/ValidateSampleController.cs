2b78c0f48dc5b5eeea5b4a04aea42dd25ba5ba9d
zackhuang <huangya0@hotmail.com>
2019-04-24 23:30:39 +0800

    Asp.net MVC利用Ajax.BeginForm实现bootstrap模态框弹出，并进行前段验证

---
+++ AspNetMvcAjaxWebApp1/AspNetMvcAjaxWebApp1/Controllers/ValidateSampleController.cs
@@ -0,0 +1,51 @@
+﻿using AspNetMvcAjaxWebApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvcAjaxWebApp1.Controllers
+{
+    public class ValidateSampleController : Controller
+    {
+        //http://jingpin.jikexueyuan.com/article/56983.html
+
+        // GET: ValidateSample
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Person()
+        {
+            return PartialView("_Person");
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Person(Person model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = "验证不通过!"; // GetErrorsFromModelState();
+                return Json(new { success = false, errors = errors });
+                //return PartialView("_Person", model);
+            }
+
+            ////save to persistent store;
+            ////return data back to post OR do a normal MVC Redirect....
+            //return Json(new { success = true });  //perhaps you want to do more on return.... otherwise this if block is not necessary....
+            ////return RedirectToAction("Index");
+
+            if (Request.IsAjaxRequest())
+            {
+                return Json(new { success = true });
+                //return JavaScript("addContentToDiv();");
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
