33638431f509ba3322ac5822939b5bb251335289
zackhuang <huangya0@hotmail.com>
2019-05-12 21:48:34 +0800

    comment

--- EFCore_Test1/ClassLibrary1/Readme.cs
+++ EFCore_Test1/ClassLibrary1/Readme.cs
@@ -3,6 +3,7 @@
 namespace ClassLibrary1
 {
     //https://www.cnblogs.com/stulzq/p/7717873.html?utm_source=debugrun&utm_medium=referral
+    //Scaffold-DbContext "Data Source=.;Initial Catalog=EmsWebDB;Integrated Security=True" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models
 
     public class Readme
     {
